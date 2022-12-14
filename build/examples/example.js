1665eb79d012c04cc3b494714a6bb9f0d702bcc7
Tomas Kraus <tomas.kraus.dev@gmail.com>
2022-12-14 02:25:05 +0100

    docs: add example

---
+++ build/examples/example.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const maplinemachine_1 = require("../src/maplinemachine");
+const node_process_1 = require("node:process");
+const toUpper = async (s) => s.toUpperCase();
+const lineMachine = (0, maplinemachine_1.mapLineMachine)(toUpper);
+const runner = async () => {
+    const res = await lineMachine('./examples/input.txt', node_process_1.stdout);
+    console.log(res);
+};
+runner();
+//# sourceMappingURL=example.js.map
---
+++ examples/example.ts
@@ -0,0 +1,11 @@
+import {mapLineMachine} from '../src/maplinemachine';
+import {stdout} from 'node:process';
+
+const toUpper = async (s: string) => s.toUpperCase();
+const lineMachine = mapLineMachine(toUpper);
+
+const runner = async () => {
+  const res = await lineMachine('./examples/input.txt', stdout);
+  console.log(res);
+};
+runner();
