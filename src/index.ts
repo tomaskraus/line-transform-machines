88d4621364d3e7c187ccdf00595fb55d3ad596cf
Tomas Kraus <tomas.kraus.dev@gmail.com>
2023-01-15 23:19:02 +0100

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export type {
+  TFileLineContext,
+  TLineMachineOptions,
+} from './line_machine_common';
+export type {TFileProcessor} from './utils/file_stream_wrapper';
 export * from './line_machine';
 export * from './async_line_machine';
 export * from './rxjs_line_machine';
