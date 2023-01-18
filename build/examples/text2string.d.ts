caafc0b0b5019da8ae525d9b8e5f9b7527b97a9c
Tomas Kraus <tomas.kraus.dev@gmail.com>
2023-01-18 23:11:07 +0100

    chore(build): update files built

--- build/examples/text2string.d.ts
+++ build/examples/text2string.d.ts
@@ -1,8 +1,13 @@
 /**
+ * TLDR: reads input text and prints a JS string that represents that input text.
+ *
  * Reads the text from a standard input. (let's call that text INPUT-TEXT)
  * Prints a piece of JS code to standard output.
  *   That JS code consists of string (or concatenation of strings) - call it OUT-STRING.
  * OUT-STRING represents an INPUT-TEXT in such a form,
  * that when OUT-STRING is printed by some JS script, its output should contain the INPUT-TEXT.
+ *
+ * Try it by running on this file:)
+ *   cat examples/text2string.ts | node build/examples/text2string.js
  */
 export {};
--- build/examples/text2string.js
+++ build/examples/text2string.js
@@ -1,10 +1,15 @@
 "use strict";
 /**
+ * TLDR: reads input text and prints a JS string that represents that input text.
+ *
  * Reads the text from a standard input. (let's call that text INPUT-TEXT)
  * Prints a piece of JS code to standard output.
  *   That JS code consists of string (or concatenation of strings) - call it OUT-STRING.
  * OUT-STRING represents an INPUT-TEXT in such a form,
  * that when OUT-STRING is printed by some JS script, its output should contain the INPUT-TEXT.
+ *
+ * Try it by running on this file:)
+ *   cat examples/text2string.ts | node build/examples/text2string.js
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_line_machine_1 = require("../src/rxjs_line_machine");
